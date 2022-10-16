700092220c09c0f9ac4bfff6062afe0f8d72886d
HubMarcus <a1852054@adelaide.edu.au>
2022-10-16 12:23:22 +1030

    slight changes. Still has compile error

--- moves_test.cpp
+++ moves_test.cpp
@@ -10,56 +10,49 @@
 int main() {
   srand(time(NULL));  // randomize seed
   // due to the randomness involved, these tests have to be checked by a human
-
-  Attack_Move* c;
-  c = new Attack_Move("basic attack", 10, 10, 70);
-
-  Heal_Move* d;
-  d = new Heal_Move("basic heal", 7, 20, 2);
-
-  Status_Move* e;
-  e = new Status_Move("generic status", 5, nullptr, "a status condition", 100);
-
-  Special_Attack_Move* f;
-  f = new Special_Attack_Move("basic special attack", 2, 12, 100, nullptr,
-                              "a status condition", 50);
+  
+  Move ** movesList = new Move*[4];
+  movesList[0] = new Attack_Move("basic_attack", 10, 10, 70);
+  movesList[1] = new Heal_Move("basic_heal", 7, 20, 2);
+  movesList[2] = new Status_Move("generic_status", 5, nullptr, "a_status_condition", 100);
+  movesList[3] = new Special_Attack_Move("basic_special_attack", 2, 12, 100, nullptr, "a_status_condition", 50);
 
   Character *a, *b;
-  a = new Character("Char1", 100, 20, 20, 50, 10, nullptr, 4);
-  b = new Character("Char2", 100, 20, 12, 30, 10, nullptr, 4);
+  a = new Character("Char1", 100, 20, 20, 50, 10, movesList, 4);
+  b = new Character("Char2", 100, 20, 12, 30, 10, movesList, 4);
 
   // test 1
-  c->Execute(a, b);
+  movesList[0]->Execute(a, b);
 
   std::cout << b->HP << "/" << b->maxHP << std::endl;
 
-  d->Execute(b, b);
+  movesList[1]->Execute(b, b);
 
   std::cout << b->HP << "/" << b->maxHP << std::endl << std::endl;
 
   // test 2
-  c->Execute(a, b);
+  movesList[0]->Execute(a, b);
 
   std::cout << b->HP << "/" << b->maxHP << std::endl;
 
-  c->Execute(a, b);
+  movesList[0]->Execute(a, b);
 
   std::cout << b->HP << "/" << b->maxHP << std::endl;
 
-  d->Execute(b, b);
+  movesList[1]->Execute(b, b);
 
   std::cout << b->HP << "/" << b->maxHP << std::endl;
 
-  d->Execute(b, b);
+  movesList[1]->Execute(b, b);
 
   std::cout << b->HP << "/" << b->maxHP << std::endl << std::endl;
 
   // test 3
-  e->Execute(a, b);
+  movesList[2]->Execute(a, b);
   std::cout << std::endl;
 
   // test 4
-  f->Execute(a, b);
+  movesList[3]->Execute(a, b);
 
   return 0;
 }
